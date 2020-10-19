57661c5bb141b9a2f72a95907bc7799dfc023a10
mikedilulo <mikedilulo2019@gmail.com>
2020-10-18 21:38:32 -0600

    Momentum Quote Fixed Bottom

--- client/src/views/Momentum.vue
+++ client/src/views/Momentum.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="momentum">
     <momentumNavbar />
+    <momentumQuote />
   </div>
 </template>
 
 <script>
 import momentumNavbar from "@/components/MomentumNavbar.vue";
+import momentumQuote from "@/components/MomentumQuote.vue";
 export default {
   name: "momentum",
   components: {
-    momentumNavbar
+    momentumNavbar,
+    momentumQuote
   }
 };
 </script>
