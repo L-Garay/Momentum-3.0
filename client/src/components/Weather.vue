381c8943a384aa3a8df190ca383c07be859e0d7d
Logan Garay <garay.logan@gmail.com>
2020-12-02 11:29:24 -0700

    added dynamic weather styling

--- client/src/components/Weather.vue
+++ client/src/components/Weather.vue
@@ -1,11 +1,42 @@
 <template>
-  <div class="main ml-auto">
+  <div
+    class="main ml-auto"
+    id="changeColor"
+    :style="{ 'background-color': backgroundColor, color: textColor }"
+  >
     <div class="background">
       <div class="content">
         <h1 class="Condition">
-          <div>
-            <i class="fas fa-sun icon"></i>
+          <div v-if="sunny">
+            <i class="fas fa-sun icon" :style="{ color: iconColor }"></i>
             {{ weather.weather[0].main }}
+          </div>
+          <div v-else-if="rain">
+            <i
+              class="fas fa-cloud-showers-heavy icon"
+              :style="{ color: iconColor }"
+            ></i>
+            {{ weather.weather[0].main }}
+          </div>
+          <div v-else-if="cloudy">
+            <i class="fas fa-cloud icon" :style="{ color: iconColor }"></i>
+            {{ weather.weather[0].main }}
+          </div>
+          <div v-else-if="snow">
+            <i class="far fa-snowflake icon" :style="{ color: iconColor }"></i>
+            {{ weather.weather[0].main }}
+          </div>
+          <div v-else-if="fog">
+            <i class="fas fa-smog icon" :style="{ color: iconColor }"></i>
+            {{ weather.weather[0].main }}
+          </div>
+          <div v-else-if="mist">
+            <i class="fas fa-smog icon" :style="{ color: iconColor }"></i>
+            {{ weather.weather[0].main }}
+          </div>
+          <div v-else>
+            <i class="fas fa-question icon" :style="{ color: iconColor }"></i>
+            <p>Unkown Weather Condition</p>
           </div>
         </h1>
         <h1 class="Temp">
@@ -29,7 +60,10 @@
             class="locationInput"
           />
           <h1 class="Location" v-else @click="getNewCity">
-            <i class="fas fa-map-marker-alt locationIcon"></i>
+            <i
+              class="fas fa-map-marker-alt locationIcon"
+              :style="{ color: locationIconColor }"
+            ></i>
             {{ weather.name }}
           </h1>
         </div>
@@ -46,6 +80,7 @@ export default {
   name: 'Weather',
   data() {
     return {
+      // To get weather
       coord: {
         lat: null,
         lon: null,
@@ -54,8 +89,22 @@ export default {
         changeCity: false,
         name: '',
       },
+      // For Date
       day: '',
       month: '',
+      // Possible weather conditions
+      sunny: false,
+      rain: false,
+      snow: false,
+      cloudy: false,
+      fog: false,
+      mist: false,
+      unkownCondition: false,
+      // For widget styling
+      backgroundColor: '',
+      textColor: 'black',
+      iconColor: '',
+      locationIconColor: 'black',
     };
   },
   mounted() {
@@ -79,6 +128,7 @@ export default {
       this.coord.lon = position.coords.longitude.toString();
       let coords = { ...this.coord };
       await this.$store.dispatch('getWeather', coords);
+      this.checkCondition();
     },
     error(err) {
       console.warn(`Error code: ${err.code}, ${err.message}.`);
@@ -91,6 +141,7 @@ export default {
     async submitNewCity() {
       await this.$store.dispatch('getNewWeather', this.city);
       this.city.changeCity = false;
+      this.checkCondition();
     },
     // Get current date
     getDate() {
@@ -115,6 +166,156 @@ export default {
     onClickOutside() {
       this.city.changeCity = false;
     },
+    // Check the weather condition to then style the widget accordingly
+    checkCondition() {
+      switch (this.$store.state.weather.weather[0].main) {
+        case 'Clear':
+          this.sunny = true;
+          this.cloudy = false;
+          this.rain = false;
+          this.snow = false;
+          this.fog = false;
+          this.mist = false;
+          this.textColor = 'black';
+          break;
+        case 'Clouds':
+          this.cloudy = true;
+          this.sunny = false;
+          this.rain = false;
+          this.snow = false;
+          this.fog = false;
+          this.mist = false;
+          this.textColor = 'white';
+          break;
+        case 'Rain':
+          this.rain = true;
+          this.cloudy = false;
+          this.sunny = false;
+          this.snow = false;
+          this.fog = false;
+          this.mist = false;
+          this.textColor = 'white';
+          break;
+        case 'Snow':
+          this.snow = true;
+          this.cloudy = false;
+          this.sunny = false;
+          this.rain = false;
+          this.fog = false;
+          this.mist = false;
+          this.textColor = 'black';
+          break;
+        case 'Fog':
+          this.fog = true;
+          this.cloudy = false;
+          this.sunny = false;
+          this.snow = false;
+          this.rain = false;
+          this.mist = false;
+          this.textColor = 'white';
+          break;
+        case 'Mist':
+          this.fog = false;
+          this.cloudy = false;
+          this.sunny = false;
+          this.snow = false;
+          this.rain = false;
+          this.mist = true;
+          this.textColor = 'white';
+          break;
+        default:
+          this.unkownCondition = true;
+      }
+      this.setBackgroundColor();
+    },
+    setBackgroundColor() {
+      // NOTE THE MAIN BACKGROUND COLOR AND TEXT COLOR DON'T CHANGE BECUASE THE ELEMENT THAT SETS THE COLORS NEVER GET'S RE-RENDERED AFTER NEW CITY IS FOUND...ONLY THE ICON COLOR AND ACTUAL DATA GET'S CHANGED
+      // NOTE WORKAROUND IS TO 'FORCE' CHANGE THE COLORS USING getElementById
+      // Sunny
+      if (this.sunny == true && this.$store.state.weather.main.temp > 100) {
+        let change = document.getElementById('changeColor');
+        change.style.backgroundColor = 'rgb(248, 78, 35)';
+        this.iconColor = 'rgb(252, 196, 15)';
+      } else if (
+        this.sunny == true &&
+        this.$store.state.weather.main.temp >= 70
+      ) {
+        let change = document.getElementById('changeColor');
+        change.style.backgroundColor = 'rgb(248, 181, 35)';
+        this.iconColor = 'rgb(252, 62, 29)';
+      } else if (
+        this.sunny == true &&
+        this.$store.state.weather.main.temp > 50
+      ) {
+        let change = document.getElementById('changeColor');
+        change.style.backgroundColor = 'rgb(253, 253, 61)';
+        this.iconColor = 'rgb(252, 155, 29)';
+      } else if (
+        this.sunny == true &&
+        this.$store.state.weather.main.temp > 0
+      ) {
+        // this.backgroundColor = "rgb(241, 241, 126)";
+        // this.iconColor = "rgb(252, 161, 41)";
+        let change = document.getElementById('changeColor');
+        change.style.backgroundColor = 'rgb(241, 241, 126)';
+        this.iconColor = 'rgb(252, 161, 41)';
+      } else if (
+        this.sunny == true &&
+        this.$store.state.weather.main.temp <= 0
+      ) {
+        let change = document.getElementById('changeColor');
+        change.style.backgroundColor = 'rgb(241, 241, 188)';
+        this.iconColor = 'rgb(252, 161, 41)';
+      }
+
+      // Rain
+      if (this.rain) {
+        let change = document.getElementById('changeColor');
+        change.style.backgroundColor = 'deepskyblue';
+        this.locationIconColor = 'grey';
+        this.iconColor = 'grey';
+      }
+
+      // Snow
+      if (this.snow == true) {
+        let change = document.getElementById('changeColor');
+        change.style.backgroundColor = 'white';
+        this.iconColor = 'black';
+      }
+
+      // Cloudy
+      if (this.cloudy) {
+        let change = document.getElementById('changeColor');
+        change.style.backgroundColor = 'grey';
+        this.iconColor = 'white';
+        this.locationIconColor = 'white';
+      }
+
+      // Fog
+      if (this.fog) {
+        let change = document.getElementById('changeColor');
+        change.style.backgroundColor = 'lightslategrey';
+        // this.textColor = "white";
+        this.iconColor = 'rgb(206, 206, 202)';
+        this.locationIconColor = 'rgb(206, 206, 202)';
+      }
+
+      // Mist
+      if (this.mist) {
+        let change = document.getElementById('changeColor');
+        change.style.backgroundColor = 'lightblue';
+        // this.textColor = "white";
+        this.iconColor = ' rgb(172, 172, 172)';
+        this.locationIconColor = ' rgb(172, 172, 172)';
+      }
+      // Unknown
+      if (this.unkownCondition) {
+        let change = document.getElementById('changeColor');
+        change.style.backgroundColor = 'white';
+        this.iconColor = 'black';
+        this.locationIconColor = 'black';
+      }
+    },
   },
   components: {},
 };
@@ -133,7 +334,7 @@ export default {
   background-color: coral;
 }
 
-/* Content */
+/* Widget styling */
 
 .icon {
   z-index: 1000;
