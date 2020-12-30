9a878b58b9868eeaa02e9ca94c0dd7fe1ce918bd
Logan Garay <garay.logan@gmail.com>
2020-12-30 10:46:35 -0700

    started turning finance tabs into components

---
+++ client/src/components/FinanceWinLose.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="winnersLosers">
+    <div class="header">
+      <h4>Day Winners and Losers</h4>
+    </div>
+    <div class="winLoseTable">
+      <table>
+        <tr>
+          <th>Symbol</th>
+          <th>Stock Name</th>
+          <th>Current Price</th>
+          <th>Day High</th>
+          <th>Day Low</th>
+          <th>% Change</th>
+          <th>Exchange Name</th>
+        </tr>
+        <finance-table
+          v-for="stock in financeWinnersLosers"
+          :key="stock.symbol"
+          :stockData="stock"
+        />
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+import FinanceTable from '@/components/FinanceWinLoseTable.vue';
+export default {
+  name: 'FinanceWinLose',
+  components: { FinanceTable },
+  computed: {
+    financeWinnersLosers() {
+      return this.$store.state.financeWinnersLosers;
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Winners/Losers styling */
+.header h4 {
+  text-align: center;
+  position: relative;
+  padding-bottom: 5px;
+}
+.header h4::after {
+  position: absolute;
+  content: '';
+  width: 60%;
+  height: 2px;
+  left: 20%;
+  bottom: 0;
+  background: white;
+}
+.winLoseTable {
+  max-height: 400px;
+  overflow-y: auto;
+}
+</style>
