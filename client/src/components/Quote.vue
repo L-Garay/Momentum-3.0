32f484a2ccc577e5edcb3d10396c46c941761a90
Logan Garay <garay.logan@gmail.com>
2021-01-15 14:59:41 -0700

    only partially working quote movement

--- client/src/components/Quote.vue
+++ client/src/components/Quote.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="quote">
-    <p>{{ Quote.quote || 'Loading...' }}</p>
-    <p>Author: {{ Quote.author || 'Loading...' }}</p>
+    <transition name="slide">
+      <p @mouseover="displayAuthor">{{ Quote.quote || 'Loading...' }}</p>
+    </transition>
+    <transition name="fade">
+      <p v-show="showAuthor">Author: {{ Quote.author || 'Loading...' }}</p>
+    </transition>
   </div>
 </template>
 
@@ -9,7 +13,9 @@
 export default {
   name: 'Quote',
   data() {
-    return {};
+    return {
+      showAuthor: false,
+    };
   },
   mounted() {
     this.$store.dispatch('getQuote');
@@ -17,6 +23,15 @@ export default {
   computed: {
     Quote() {
       return this.$store.state.quote.quote;
+    },
+  },
+  methods: {
+    displayAuthor() {
+      if (this.showAuthor == true) {
+        this.showAuthor = false;
+      } else if (this.showAuthor == false) {
+        this.showAuthor = true;
+      }
     },
   },
 };
@@ -29,18 +44,39 @@ export default {
 p {
   font-size: 20px;
   color: white;
+  text-shadow: 3px 3px 5px black;
 }
 p:first-child {
   margin: 0;
-  text-shadow: 3px 3px 5px black;
 }
+/* p:first-child:hover {
+  transform: translate3d(0px, 11px, 0px);
+} */
 p:last-child {
-  opacity: 0.7;
+  opacity: 0.85;
 }
-p:last-child:hover {
-  opacity: 1;
-  text-shadow: 3px 3px 5px black;
+/* .slide-enter-active {
+  transition: all 0.4s ease;
 }
+.slide-leave-active {
+  transition: all 0.4s ease;
+}
+.slide-enter, .slide-leave-to {
+  transform: translate3d(10px);
+  opacity: 0;
+} */
+.fade-enter-active {
+  transition: all 0.4s ease;
+}
+.fade-leave-active {
+  transition: all 0.4s cubic-bezier(1, 0.5, 0.8, 1);
+}
+.fade-enter, .fade-leave-to
+/* .fade-leave-active below version 2.1.8 */ {
+  transform: translateY(-10px);
+  opacity: 0;
+}
+
 @media (max-width: 1024px) {
   p {
     font-size: 15px;
