9b05bf313370f58612c9122d6ea4749539c3be92
Logan Garay <garay.logan@gmail.com>
2021-01-16 14:40:32 -0700

    quote is animated

--- client/src/components/Quote.vue
+++ client/src/components/Quote.vue
@@ -1,11 +1,19 @@
 <template>
-  <div class="quote">
-    <transition name="slide">
-      <p @mouseover="displayAuthor">{{ Quote.quote || 'Loading...' }}</p>
-    </transition>
-    <transition name="fade">
-      <p v-show="showAuthor">Author: {{ Quote.author || 'Loading...' }}</p>
-    </transition>
+  <div
+    class="Quote"
+    @mouseover="showAuthor = true"
+    @mouseleave="showAuthor = false"
+  >
+    <div class="center">
+      <p class="quote">
+        {{ '"' + Quote.quote + '"' || 'Loading...' }}
+      </p>
+      <transition name="fade">
+        <p v-show="showAuthor" class="author">
+          Author: {{ Quote.author || 'Loading...' }}
+        </p>
+      </transition>
+    </div>
   </div>
 </template>
 
@@ -25,55 +33,57 @@ export default {
       return this.$store.state.quote.quote;
     },
   },
-  methods: {
-    displayAuthor() {
-      if (this.showAuthor == true) {
-        this.showAuthor = false;
-      } else if (this.showAuthor == false) {
-        this.showAuthor = true;
-      }
-    },
-  },
+  methods: {},
 };
 </script>
 
 <style scoped>
-.quote {
+.Quote {
   text-align: center;
+  padding-bottom: 10px;
+}
+.center {
+  transition: transform 0.35s ease-in-out;
+}
+.Quote:hover .center {
+  transform: translate3d(0px, -10px, 0px);
+}
+.Quote:hover p.quote {
+  padding-bottom: 0;
+}
+.Quote:hover p.author {
+  margin-top: 0;
 }
 p {
   font-size: 20px;
   color: white;
   text-shadow: 3px 3px 5px black;
+  margin-bottom: 0;
 }
-p:first-child {
-  margin: 0;
+p.quote {
+  padding-bottom: 5px;
 }
-/* p:first-child:hover {
-  transform: translate3d(0px, 11px, 0px);
-} */
-p:last-child {
-  opacity: 0.85;
+
+p.author {
+  margin-top: -25px;
+  font-size: 15px;
 }
-/* .slide-enter-active {
-  transition: all 0.4s ease;
-}
-.slide-leave-active {
-  transition: all 0.4s ease;
-}
-.slide-enter, .slide-leave-to {
-  transform: translate3d(10px);
-  opacity: 0;
-} */
+
 .fade-enter-active {
-  transition: all 0.4s ease;
+  transition: all 0.35s ease;
+  opacity: 0.05;
 }
 .fade-leave-active {
-  transition: all 0.4s cubic-bezier(1, 0.5, 0.8, 1);
+  transition: all 0.35s ease;
+  opacity: 0.05;
 }
-.fade-enter, .fade-leave-to
-/* .fade-leave-active below version 2.1.8 */ {
-  transform: translateY(-10px);
+.fade-leave,
+.fade-enter-to {
+  opacity: 1;
+}
+.fade-enter,
+.fade-leave-to {
+  transform: translateY(-8px);
   opacity: 0;
 }
 
