b5d8349936a68922f5477ae5b6b64c2296a81269
Logan Garay <garay.logan@gmail.com>
2021-01-20 16:20:11 -0700

    created basic test game component

--- client/src/views/Momentum.vue
+++ client/src/views/Momentum.vue
@@ -8,12 +8,18 @@
     <div class="container-fluid top">
       <div class="row">
         <div class="col-1">
-          <div v-if="toggledNews" class="toggleNews" @click="openNewsModal">
+          <div v-if="toggledNews" class="toggle" @click="openNewsModal">
             <i class="far fa-newspaper fa-2x"></i>
-            <p>NEWS</p>
+            <p>News</p>
           </div>
         </div>
-        <div class="col-3 offset-8">
+        <div class="col-1">
+          <div v-if="toggledGames" class="toggle games" @click="openGames">
+            <i class="far fa-newspaper fa-2x"></i>
+            <p>Games</p>
+          </div>
+        </div>
+        <div class="col-3 offset-7">
           <weather />
         </div>
         <div class="col-1">
@@ -37,9 +43,7 @@
           @close-quotes-modal="closeQuotesModal"
         />
         <news-modal v-if="showNewsModal" @close-news-modal="closeNewsModal" />
-        <!-- <div class="col-3">
-          <calculator v-if="toggledCalculator" class="calculator" />
-        </div> -->
+        <game v-if="showGames" @close-games="closeGames" />
         <!-- NOTE Can't use bootstrap modal because for some reason I get an error saying the '$' symbol when trying to programatically open/close it using bootstrap's $('#myModal').modal(options) is 'undefined'. I have tried using a cdn directly from jquery, I have tried npm i-ing jquery directly into the project and neither work.  I have tried reordering the cdns, I have tried nmp i-ing bootstrap directly, and neither work.  I have no idea why it's not working. So the workaround is to use a modal made by the vue devs found at https://vuejs.org/v2/examples/modal.html?
           <div
           class="modal"
@@ -92,7 +96,7 @@
           <quote />
         </div>
         <div class="col-1 offset-2">
-          <p class="toggle" @click="toggleUtilities">Utilities</p>
+          <p class="utilities" @click="toggleUtilities">Utilities</p>
           <transition name="fade">
             <div v-if="showUtilities">
               <utilities /></div
@@ -118,10 +122,10 @@ import Clock from '@/components/Clock.vue';
 import Greeting from '@/components/Greeting.vue';
 import PhotoModal from '@/components/PhotoModal.vue';
 import QuoteModal from '@/components/QuoteModal.vue';
-// import Todo from '@/components/Utilities/Todo.vue';
 import Calculator from '@/components/Calculator.vue';
 import NewsModal from '@/components/NewsModal.vue';
 import Utilities from '@/components/Utilities/Utilities.vue';
+import Game from '@/components/GameComponent.vue';
 
 export default {
   name: 'momentum',
@@ -133,10 +137,10 @@ export default {
     Greeting,
     PhotoModal,
     QuoteModal,
-    // Todo,
     Calculator,
     NewsModal,
     Utilities,
+    Game,
   },
   data() {
     return {
@@ -147,6 +151,8 @@ export default {
       showCalculator: false,
       toggledNews: false,
       showNewsModal: false,
+      toggledGames: true,
+      showGames: false,
       showUtilities: false,
       gotPhoto: false,
     };
@@ -230,6 +236,13 @@ export default {
         this.toggledCalculator = false;
       }
     },
+    // Games control
+    openGames() {
+      this.showGames = true;
+    },
+    closeGames() {
+      this.showGames = false;
+    },
     // Utilities control
     toggleUtilities() {
       if (this.showUtilities == false) {
@@ -273,12 +286,12 @@ export default {
 .todoList {
   text-align: center;
 }
-p.toggle {
+p.utilities {
   color: white;
   font-size: 20px;
   text-shadow: 3px 3px 3px black;
 }
-p.toggle:hover {
+p.utilities:hover {
   cursor: pointer;
   font-size: 25px;
   text-shadow: 5px 5px 3px black;
@@ -290,9 +303,8 @@ p.toggle:hover {
 .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
   opacity: 0;
 }
-
-/* News styling */
-div.toggleNews {
+/* Toggle element styling */
+.toggle {
   color: black;
   background-color: rgba(255, 255, 255, 0.5);
   border: 1pt solid black;
@@ -303,14 +315,19 @@ div.toggleNews {
   margin-top: 17px;
   margin-right: 20px;
 }
-div.toggleNews:hover {
+div.toggle:hover {
   border: solid 2pt black;
   background-color: white;
   cursor: pointer;
   font-weight: bold;
 }
-div.toggleNews p {
+div.toggle p {
   font-size: 12px;
   text-shadow: 1pt 1pt 1pt white;
+}
+
+/* Games styling */
+.toggle.games {
+  margin-left: -65px;
 }
 </style>
